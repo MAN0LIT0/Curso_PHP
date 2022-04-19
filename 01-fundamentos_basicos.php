5eb42ec54cf96a478ce07fff9218028e38cf42cf
Marco Gondim <marcoantonioantonio949@gmail.com>
2022-04-19 15:48:50 -0300

    commit inicial

---
+++ 01-fundamentos_basicos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>fundamentos basicos php</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php
+    echo "meu nome é Marco";
+?>
+</body>
+</html>
